e00a2b0aeb10ce17700f68cbcc7fd1b327552d81
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2021-09-29 09:31:19 -0500

    Due Date

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ import main;
 import datetime;
 
 year = 2021
-month = 1
-day = 5
+month = 10
+day = 8
 
 def test_code():
     assert main.shareFair(10, 2) == True, " shareFair(10, 2) == True failed"
